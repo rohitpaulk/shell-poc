daf3444a7a5c175c0d7f99f20b498e27300b5cfb
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-05-09 22:36:01 +0100

    Refactor main.rb: Add command handling function and implement 'which' command.

--- main.rb
+++ main.rb
@@ -1,13 +1,28 @@
-loop do
-  $stdout.write("$ ")
-  command, *args = gets.chomp.split(" ")
-
+def handle_command(command, args)
   case command
   when "exit"
-    break
+    exit 0
   when "echo"
     puts args.join(" ")
+  when "which"
+    matching_path_entry = ENV["PATH"].split(":").detect do |path|
+      full_path = File.join(path, args.first)
+
+      File.executable?(full_path) ? full_path : nil
+    end
+
+    if matching_path_entry
+      puts File.join(matching_path_entry, args.first)
+    else
+      puts "#{args.first} not found"
+    end
   else
     puts "#{command}: command not found"
   end
+end
+
+loop do
+  $stdout.write("$ ")
+  command, *args = gets.chomp.split(" ")
+  handle_command(command, args)
 end
